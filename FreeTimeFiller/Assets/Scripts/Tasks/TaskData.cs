ccec21590a1bca62bbc63b0950e31663f900c4f9
RussellA152 <russellayala152@gmail.com>
2024-01-28 17:21:25 -0800

    Added Task and TaskData scripts
    TaskData is a scriptableObject that stores  the details about a task such as description, difficulty level, and category.

---
+++ FreeTimeFiller/Assets/Scripts/Tasks/TaskData.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Task Data", menuName = "ScriptableObjects/Task Data")]
+public class TaskData : ScriptableObject
+{
+    public string name;
+
+    [Space(10)]
+    public Category category;
+    
+    [Range(0, 5)]
+    public int difficultyLevel;
+    
+    [TextArea(2,3), Space(10)]
+    public string description;
+}
+
+public enum Category
+{
+    Sports,
+    Chores,
+    Hobby,
+    Fun,
+    Healthy,
+    School,
+    Workout,
+    Other
+}
