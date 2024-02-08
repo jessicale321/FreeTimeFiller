544bcbeeb49689226850ed6ea643217f8e267cb2
jessicale321 <jessicale321@gmail.com>
2024-02-08 09:31:45 -0800

    Navigate from login to main scene

---
+++ FreeTimeFiller/Assets/Scripts/LoginMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginMenu : MonoBehaviour
+{
+    public void OnLoginButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
