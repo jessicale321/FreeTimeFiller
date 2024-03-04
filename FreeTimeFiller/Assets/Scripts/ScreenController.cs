0337289d655dfd584e5e7a044b207fbd652b24bc
jessicale321 <jessicale321@gmail.com>
2024-03-04 12:01:57 -0800

    UI manager architecture

--- FreeTimeFiller/Assets/Scripts/ScreenController.cs
+++ FreeTimeFiller/Assets/Scripts/ScreenController.cs
@@ -7,21 +7,22 @@ using UnityEngine.UI;
 ///
 public class ScreenController : MonoBehaviour
 {
-    [SerializeField] private GameObject homeScreen;
-    [SerializeField] private GameObject searchScreen;
-    [SerializeField] private GameObject profileScreen;
+    [SerializeField] private MenuScreen homeScreen;
+    [SerializeField] private MenuScreen searchScreen;
+    [SerializeField] private MenuScreen profileScreen;
 
     [SerializeField] private Button homeButton;
     [SerializeField] private Button searchButton;
     [SerializeField] private Button profileButton;
 
+    private MenuScreen lastShownScreen;
+
     //-/////////////////////////////////////////////////////////////////////
     ///
     public void Start()
     {
-        homeScreen.SetActive(true);
-        searchScreen.SetActive(false);
-        profileScreen.SetActive(false);
+        homeScreen.Show();
+        lastShownScreen = homeScreen;
 
         homeButton.onClick.AddListener(OnHomeButtonClicked);
         searchButton.onClick.AddListener(OnSearchButtonClicked);
@@ -32,26 +33,26 @@ public class ScreenController : MonoBehaviour
     ///
     public void OnHomeButtonClicked()
     {
-        homeScreen.SetActive(true);
-        searchScreen.SetActive(false);
-        profileScreen.SetActive(false);
+        lastShownScreen.Hide();
+        homeScreen.Show();
+        lastShownScreen = homeScreen;
     }
 
     //-/////////////////////////////////////////////////////////////////////
     ///
     public void OnSearchButtonClicked()
     {
-        homeScreen.SetActive(false);
-        searchScreen.SetActive(true);
-        profileScreen.SetActive(false);
+        lastShownScreen.Hide();
+        searchScreen.Show();
+        lastShownScreen = searchScreen;
     }
 
     //-/////////////////////////////////////////////////////////////////////
     ///
     public void OnProfileButtonClicked()
     {
-        homeScreen.SetActive(false);
-        searchScreen.SetActive(false);
-        profileScreen.SetActive(true);
+        lastShownScreen.Hide();
+        profileScreen.Show();
+        lastShownScreen = profileScreen;
     }
 }
