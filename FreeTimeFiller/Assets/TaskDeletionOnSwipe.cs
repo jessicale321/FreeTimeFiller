410b74a00cb85b430e27453f01a6b40d41994b5c
RussellA152 <russellayala152@gmail.com>
2024-03-18 20:48:26 -0700

    Showing a delete button on task swipe

--- FreeTimeFiller/Assets/TaskDeletionOnSwipe.cs
+++ FreeTimeFiller/Assets/TaskDeletionOnSwipe.cs
@@ -3,9 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TaskDeletionOnSwipe : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
+    [SerializeField] private Button deleteButton;
+    
     private Vector2 _startPosition;
     private bool _isDragging;
 
@@ -16,6 +19,13 @@ public class TaskDeletionOnSwipe : MonoBehaviour, IPointerDownHandler, IPointerU
     [SerializeField, Range(5, 50f)] private float swipeThreshold = 5f;
 
     private void OnEnable()
+    {
+        deleteButton.gameObject.SetActive(false);
+        _swipingLeft = false;
+        _swipingLeft = false;
+    }
+
+    private void OnDisable()
     {
         _swipingLeft = false;
         _swipingLeft = false;
@@ -47,20 +57,42 @@ public class TaskDeletionOnSwipe : MonoBehaviour, IPointerDownHandler, IPointerU
             // Check if user swiped right
             if (direction.x > 0 && !_swipingRight)
             {
-                Debug.Log("Swiped right on delete button");
-                _swipingRight = true;
-                _swipingLeft = false;
+                OnSwipeRight();
             }
             // Check if user swiped left
             else if (direction.x < 0 && !_swipingLeft)
             {
-                Debug.Log("Swiped left on delete button");
-                _swipingLeft = true;
-                _swipingRight = false;
+                OnSwipeLeft();
             }
 
             // Reset isDragging after detecting a swipe
             _isDragging = false;
         }
+    }
+
+    ///-///////////////////////////////////////////////////////////
+    /// When the user swipes left, show the deletion button.
+    /// 
+    private void OnSwipeLeft()
+    {
+        Debug.Log("Swiped left on delete button");
+        _swipingLeft = true;
+        _swipingRight = false;
+                
+        // Show delete button
+        deleteButton.gameObject.SetActive(true);
+    }
+
+    ///-///////////////////////////////////////////////////////////
+    /// When the user swipes right, hide the deletion button.
+    /// 
+    private void OnSwipeRight()
+    {
+        Debug.Log("Swiped right on delete button");
+        _swipingRight = true;
+        _swipingLeft = false;
+                
+        // Hide delete button
+        deleteButton.gameObject.SetActive(false);
     }
 }
