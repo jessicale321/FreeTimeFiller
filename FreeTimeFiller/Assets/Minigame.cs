89ea621c614edb1112d5579fa97cd4d6cb50eddd
Russell Ayala <russellayala152@gmail.com>
2024-04-18 11:30:25 -0700

    Fixed minigame script bugs

--- FreeTimeFiller/Assets/Minigame.cs
+++ FreeTimeFiller/Assets/Minigame.cs
@@ -5,9 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class Minigame : MonoBehaviour
 {
+    [SerializeField, Range(1f, 300f)] private float gameTimer;
+
+    private string _currentSceneName;
+
     private void Start()
     {
         Invoke(nameof(OnMinigameConcluded), 3f);
+    }
+
+    private void OnEnable()
+    {
+        // Subscribe to the sceneLoaded event
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnDisable()
+    {
+        // Unsubscribe from the sceneLoaded event
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    ///-///////////////////////////////////////////////////////////
+    /// Get the name of the current scene that just loaded
+    /// 
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(mode == LoadSceneMode.Additive)
+            _currentSceneName = scene.name;
     }
 
     protected virtual void OnMinigameLoaded()
@@ -17,9 +41,7 @@ public class Minigame : MonoBehaviour
 
     protected virtual void OnMinigameConcluded()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-
         // Unload the current scene (go back to home page)
-        SceneManager.UnloadSceneAsync(currentSceneIndex);
+        SceneManager.UnloadSceneAsync(_currentSceneName);
     }
 }
